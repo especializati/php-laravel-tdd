85b008c85aba909d72998bfd1d5b8c6f2fbc8731
Carlos Ferreira <carlos@especializati.com.br>
2022-06-23 15:50:18 -0300

    Handler Exception NotFoundException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use App\Repository\Exceptions\NotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +39,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof NotFoundException) {
+            return response()->json(
+                ['errors' => $exception->getMessage()],
+                Response::HTTP_NOT_FOUND
+            );
+        }
     }
 }
